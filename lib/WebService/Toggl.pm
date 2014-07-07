be11f2eb82a0c9993f5750cffdea6b9ac53925ab
Fitz Elliott <felliott@fiskur.org>
2014-07-07 14:57:56 -0400

    add fetch methods to ::Toggl

--- lib/WebService/Toggl.pm
+++ lib/WebService/Toggl.pm
@@ -10,23 +10,32 @@ our $VERSION = "0.01";
 
 
 has 'me' => (is =>'ro', lazy => 1, builder => 1);
-sub _build_me {
-    use_package_optimistically('WebService::Toggl::API::Me')
-        ->new({api_key => shift->api_key})
+sub _build_me { shift->_new_thing('::API::Me') }
+
+
+sub workspace     { shift->_new_thing_by_id('::API::Workspace',     @_) }
+sub client        { shift->_new_thing_by_id('::API::Client',        @_) }
+sub project       { shift->_new_thing_by_id('::API::Project',       @_) }
+sub project_user  { shift->_new_thing_by_id('::API::ProjectUser',   @_) }
+sub tag           { shift->_new_thing_by_id('::API::Tag',           @_) }
+sub task          { shift->_new_thing_by_id('::API::Task',          @_) }
+sub time_entry    { shift->_new_thing_by_id('::API::TimeEntry',     @_) }
+sub user          { shift->_new_thing_by_id('::API::User',          @_) }
+sub WorkspaceUser { shift->_new_thing_by_id('::API::WorkspaceUser', @_) }
+
+
+sub details { shift->_new_thing('::Report::Details', @_) }
+sub summary { shift->_new_thing('::Report::Summary', @_) }
+sub weekly  { shift->_new_thing('::Report::Weekly',  @_) }
+
+
+sub _new_thing {
+    my ($self, $class, $args) = @_;
+    return use_package_optimistically('WebService::Toggl' . $class)
+        ->new({_request => $self->_request, %{$args || {}}});
 }
 
-
-sub workspace {
-    use_package_optimistically('WebService::Toggl::API::Workspace')
-        ->new({api_key => shift->api_key, id => shift})
-}
-
-
-sub details {
-    my ($self, $args) = @_;
-    use_package_optimistically('WebService::Toggl::Report::Details')
-        ->new({api_key => shift->api_key, %{ shift() }})
-}
+sub _new_thing_by_id { shift->_new_thing(shift, {id => shift}) }
 
 1;
 __END__
