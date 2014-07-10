ca8509064dfaab45af42ea1d6989fdbfeee1a813
Fitz Elliott <felliott@fiskur.org>
2014-07-10 10:40:44 -0400

    ::Role::Item: add docs

--- lib/WebService/Toggl/Role/Item.pm
+++ lib/WebService/Toggl/Role/Item.pm
@@ -36,3 +36,64 @@ sub make_variant {
 
 1;
 __END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Toggl::Role::Item - Create roles for all WebService::Toggl::API Items
+
+=head1 SYNOPSIS
+
+ package WebService::Toggl::API::Tag;
+
+ use WebService::Toggl::Role::Item as => 'JsonItem';
+
+ use Moo;
+ with 'WebService::Toggl::API';
+ use namespace::clean;
+
+ with JsonItem(
+     bools    => [ qw()       ],
+     strings  => [ qw(name)   ],
+     integers => [ qw(id wid) ],
+ );
+
+ sub api_path { 'tags' }
+ sub api_id   { shift->id }
+
+
+=head1 DESCRIPTION
+
+This package constructs dynamic roles for WebService::Toggl::API
+objects representing individual Items.  The calling class gives it a
+list of boolean fields, a list of string fields, and a list of integer
+fields.  This package will then construct type-checked accessors for
+all the provided attributes to fetch them from the raw response.
+Calling the constructed attributes will cause an API request to be
+made, unless the raw data already exists in the object.
+
+=head2 Provided Attributes
+
+=head3 raw
+
+The raw data returned from an API request.
+
+=head2 Provided Methods
+
+=head3 my_url
+
+Returns the API URL for the object.
+
+=head1 LICENSE
+
+Copyright (C) Fitz Elliott.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Fitz Elliott E<lt>felliott@fiskur.orgE<gt>
+
+=cut
